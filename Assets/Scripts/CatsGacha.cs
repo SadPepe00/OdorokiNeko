0a6cd85bad63e74af9ffb02412008c59048b7655
immortal-flame <115133414+immortal-flame@users.noreply.github.com>
2023-10-19 19:40:49 +0300

    added GachaSystem

---
+++ Assets/Scripts/CatsGacha.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class CatsGacha : MonoBehaviour
+{
+
+    [SerializeField] private Image img;
+    [SerializeField] private TextMeshProUGUI name;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
