f2c7fa7309dcb69350fd99d059f917f639d81859
SadPepe00 <115133850+SadPepe00@users.noreply.github.com>
2023-11-23 02:13:23 +0300

    Squashed commit of the following:
    commit 787cc19ad1435e21dd57b17da5529ed4a03bad03
    Author: SadPepe00 <115133850+SadPepe00@users.noreply.github.com>
    Date:   Thu Nov 23 02:13:03 2023 +0300
    
        Main character and enemy prefabs
    
        Main character,enemy, bullet prefabs, health script, colliders logic

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float maxHealth = 100.0f;
+
+    private float currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        // Вызываем этот метод при получении урона
+        currentHealth -= amount;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        // Вызываем этот метод при смерти
+        Debug.Log("Player died!");
+        // Дополнительные действия при смерти игрока
+        // Например, перезапуск уровня, отображение экрана смерти и т.д.
+    }
+}
