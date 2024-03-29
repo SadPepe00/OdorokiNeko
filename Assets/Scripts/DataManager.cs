4722f590ad30791200bc05d93c4186e127840302
immortal-flame <113933251+nevierly@users.noreply.github.com>
2024-03-29 21:29:42 +0300

    major update
    -added 4 more cats to GACHA
    -added 1 soundtrack for lvl2
    -added INTRO.png & INTRO_BUTTON.png sprite for intro (disclaimer)
    
    it took me 5,5 hours
    
    (still wanna die btw) <--(it's a joke)
    
    i love the judges <3
    
    ((still still wanna die btw)) <--((still a joke))

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -6,7 +6,13 @@ public class DataManager : MonoBehaviour
     public int level_num = 0;
     public int player_health = 5;
     public int player_money = 200;
-    public Dictionary<string, bool> cat_collection = new Dictionary<string, bool>(){{"Буханка", false },{"Синнабон",false}, { "Рыжик", false },{ "Печенька", false }, { "Снежок", false }, { "Фантомка", false }, { "Люцикот", false }, { "НекоАрк", false } };
+    public Dictionary<string, bool> cat_collection = new Dictionary<string, bool>()
+    {{"Буханка", false },{"Синнабон",false}, 
+     {"Рыжик", false },{ "Печенька", false }, 
+     {"Снежок", false }, { "Фантомка", false }, 
+     {"Люцикот", false }, { "НекоАрк", false }, 
+     {"Снежинка", false}, { "Уголек", false },
+     {"Кошкодевочка", false}, { "Нян-Кэт", false }};
     public float soud_volume = 0;
 
     private void Awake()
